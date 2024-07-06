8b2405e5497225d0b8f299a192fa23b65eb9b68e
majidbhuiyan20 <majid.bhuiyan2011@gmail.com>
2024-07-06 12:29:59 +0600

    Search bar add in home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,10 +8,76 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("I am Home"),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff213A50),
+                  Color(0xff071938),
+                ],
+              ),
+            ),
+          ),
+          Column(//search bar col
+            children: [
+              //Search Bar
+              SafeArea(
+                child: Container(
+                // Search Container
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  margin: EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
+                  child: Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") == "") {
+                            print("Blank Search");
+                          } else {
+                            Navigator.pushReplacementNamed(context, "/loading",
+                                arguments: {
+                                  "searchText": searchController.text,
+                                });
+                          }
+                
+                  // Define your onTap action here
+                        },
+                        child: Container(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        ), // Use Icon widget here
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration: InputDecoration(
+                            hintText: "Let's Cock Something",
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Search Bar Col end
+        ],
+      ),
     );
   }
 }
